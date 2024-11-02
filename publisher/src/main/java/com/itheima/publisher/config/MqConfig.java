1f0118f9c2290505b77309913254e21b8a9a3835
Dahulu <1215765236@qq.com>
2024-11-02 21:16:36 +0800

    feat(publisher): 添加 RabbitMQ消息确认机制
    - 在 application.yml 中开启 publisher confirm 和 return机制
    - 新增 MqConfig 类，配置 ReturnCallback
    - 在 testSimpleQueue 中添加测试用例，验证消息确认功能

---
+++ publisher/src/main/java/com/itheima/publisher/config/MqConfig.java
@@ -0,0 +1,45 @@
+package com.itheima.publisher.config;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.ReturnedMessage;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author daihongli
+ * @version 1.0
+ * @ClassName MqConfig
+ * @Description: 定义ReturnCallback
+ * spring:
+ *   rabbitmq:
+ *     publisher-confirm-type: correlated # 开启publisher confirm机制，并设置confirm类型
+ *     publisher-returns: true # 开启publisher return机制
+ * @Date 2024-11-02 20:56
+ */
+@Slf4j
+@AllArgsConstructor
+@Configuration
+public class MqConfig {
+    private final RabbitTemplate rabbitTemplate;
+
+    @PostConstruct
+    public void init(){
+        rabbitTemplate.setReturnsCallback(new RabbitTemplate.ReturnsCallback() {
+            @Override
+            public void returnedMessage(ReturnedMessage returned) {
+                log.error("触发return callback,");
+                log.debug("exchange: {}", returned.getExchange());
+                log.debug("routingKey: {}", returned.getRoutingKey());
+                log.debug("message: {}", returned.getMessage());
+                log.debug("replyCode: {}", returned.getReplyCode());
+                log.debug("replyText: {}", returned.getReplyText());
+            }
+        });
+    }
+}
